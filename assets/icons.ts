52ebff36d6df66332baf746a8de418fcb6563767
Youngson335 <tolstovvlad335@gmail.com>
2025-04-29 18:22:15 +0900

    first commit

---
+++ assets/icons.ts
@@ -0,0 +1,15 @@
+import airplane from '@/assets/icons/airplane-icon.svg'
+import calendar from '@/assets/icons/calendar-icon.svg'
+import car from '@/assets/icons/car-icon.svg'
+import document from '@/assets/icons/document-icon.svg'
+import like from '@/assets/icons/like-icon.svg'
+import star from '@/assets/icons/star-icon.svg'
+
+export {
+    airplane,
+    calendar,
+    car,
+    document,
+    like,
+    star   
+}
---
+++ assets/images.ts
@@ -0,0 +1,9 @@
+import airplane from '@/assets/images/airplane-image.svg'
+import hat from '@/assets/images/hat-image.jpg'
+import maps from '@/assets/images/maps-image.svg'
+
+export  {
+    airplane,
+    hat,
+    maps
+}
